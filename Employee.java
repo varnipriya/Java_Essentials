d67e5fad205f267e950f4cfaae7dfe74cfe59efd
varnipriya <42865708+varnipriya@users.noreply.github.com>
2020-10-10 12:50:01 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,11 @@
+package com.varnipriya;
+
+public class Employee {
+    String name;
+    int age;
+    String city;
+    void display()
+    {
+        System.out.println("The name is: "+name+"\nThe age is: "+age+"\nThe city is: "+city);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.varnipriya;
+
+public class Main {
+
+    public static void main(String[] args) {
+      Employee e=new Employee();
+      e.name="priya";
+      e.age=20;
+      e.city="Nellore";
+      e.display();
+      Employee e1=new Employee();
+      e1.name="varni";
+      e1.age=20;
+      e1.city="Bengaluru";
+      e1.display();
+//      System.out.println(e.name+" "+e.age+" "+e.city);
+    }
+}
