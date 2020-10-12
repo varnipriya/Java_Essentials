4c165b615bd9292d606de63ef47523fb97e68c54
varnipriya <42865708+varnipriya@users.noreply.github.com>
2020-10-12 14:50:14 +0530

    Add files via upload
    Day 3 Assignment

--- Main.java
+++ Main.java
@@ -1,18 +1,49 @@
 package com.varnipriya;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-      Employee e=new Employee();
-      e.name="priya";
-      e.age=20;
-      e.city="Nellore";
-      e.display();
-      Employee e1=new Employee();
-      e1.name="varni";
-      e1.age=20;
-      e1.city="Bengaluru";
-      e1.display();
-//      System.out.println(e.name+" "+e.age+" "+e.city);
+        String name;
+        int date,month,year,age;
+        long salary,annual,tax;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter employee name" );
+        name=sc.nextLine();
+        System.out.println("Enter your birth date ");
+        date=sc.nextInt();
+        System.out.println("Enter your birth month");
+        month=sc.nextInt();
+        System.out.println("Enter your birth year ");
+        year=sc.nextInt();
+        System.out.println("Enter your monthly  salary ");
+        salary=sc.nextLong();
+        age=2020-year;
+        if (month>10 ){
+            age--;
+        }
+        System.out.println("Yname:"+name);
+        System.out.println("age:"+age);
+        annual=salary*12;
+        System.out.println("Annual Salary:"+annual);
+        if(annual>=500000){
+            tax=annual*20/100;
+            System.out.println("tax:"+tax);
+        }
+        else if(annual<500000 && annual>=400000){
+            tax=annual*15/100;
+            System.out.println("tax:"+tax);
+        }
+        else if(annual<400000 && annual>=300000){
+            tax=annual*10/100;
+        }
+        else if(annual<300000 && annual>=200000)
+        {
+            tax=annual*5/100;
+            System.out.println("tax:"+tax);
+        }
+        else
+        {
+            System.out.println("No tax");
+        }
     }
 }
